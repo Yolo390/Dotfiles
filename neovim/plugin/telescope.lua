7381540f468661d9c468d16e7f4a763ee61d15e7
Flo Slv <florent3969@hotmail.fr>
2022-08-09 00:39:20 +0200

    🟦 feat: Add keymap to remove buffer in telescope buffer list

--- neovim/plugin/telescope.lua
+++ neovim/plugin/telescope.lua
@@ -1,27 +1,20 @@
 require'telescope'.setup {
 	defaults = {
 		prompt_prefix = '🔍 ',
-		hidden = true
+		hidden = true,
+		mappings = {
+			n = {
+				['<C-d>'] = require'telescope.actions'.delete_buffer
+			}
+		}
 	},
 	extensions = {
 		bookmarks = {
-			-- Available: 'brave', 'buku', 'chrome', 'chrome_beta', 'edge', 'safari', 'firefox', 'vivaldi'
 			selected_browser = 'firefox',
-
-			-- Either provide a shell command to open the URL
-			url_open_command = 'open',
-
-			-- Or provide the plugin name which is already installed
-			-- Available: 'vim_external', 'open_browser'
-			url_open_plugin = nil,
-
-			-- Show the full path to the bookmark instead of just the bookmark name
+			url_open_command = 'open', -- Either provide a shell command to open the URL
+			url_open_plugin = nil, -- Available: 'vim_external', 'open_browser'
 			full_path = true,
-
-			-- Provide a custom profile name for Firefox
 			firefox_profile_name = nil,
-
-			-- Provide debug messages
 			debug = false
 		}
 	}

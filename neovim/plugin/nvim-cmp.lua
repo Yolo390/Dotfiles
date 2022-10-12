a63d4493accce5273a58d5cf162c7b6f263c3af3
Flo Slv <florent3969@hotmail.fr>
2022-10-12 10:06:23 +0200

    🟥 fix: avoid error in nvim-cmp with comment... WIP

--- neovim/plugin/nvim-cmp.lua
+++ neovim/plugin/nvim-cmp.lua
@@ -61,12 +61,12 @@ cmp.setup({
 		-- disable completion in comments
 		local context = require'cmp.config.context'
 		-- keep command mode completion enabled when cursor is in a comment
-		if vim.api.nvim_get_mode().mode == 'c' then
-			return true
-		else
-			return not context.in_treesitter_capture('comment')
-			and not context.in_syntax_group('Comment')
-		end
+		-- if vim.api.nvim_get_mode().mode == 'c' then
+		-- 	return true
+		-- else
+		-- 	return not context.in_treesitter_capture('comment')
+		-- 	and not context.in_syntax_group('Comment')
+		-- end
 	end
 })
 

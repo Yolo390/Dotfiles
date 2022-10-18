0a83b7490d2cd337418665e8550b3a57f660e156
Flo Slv <florent3969@hotmail.fr>
2022-10-18 16:28:56 +0200

    🟥 fix: nvim_cmp update function

--- neovim/plugin/nvim-cmp.lua
+++ neovim/plugin/nvim-cmp.lua
@@ -72,7 +72,7 @@ cmp.setup({
 
 -- Setup lspconfig.
 local lspconfig = require'lspconfig'
-local capabilities = require'cmp_nvim_lsp'.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require'cmp_nvim_lsp'.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local servers = {
 	'tsserver',

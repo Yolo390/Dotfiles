5e46a86ad3fde6bc87422c488f22e5793e1f73f1
Flo Slv <florent3969@hotmail.fr>
2022-10-21 08:57:49 +0200

    🟥 fix: autocompletion working. Did changes on nvim-cmp

--- neovim/plugin/nvim-cmp.lua
+++ neovim/plugin/nvim-cmp.lua
@@ -10,7 +10,7 @@ cmp.setup({
 			luasnip.lsp_expand(args.body)
 		end
 	},
-	mapping = cmp.mapping.preset.insert({
+	mapping = cmp.mapping.preset.cmdline({
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
@@ -56,18 +56,7 @@ cmp.setup({
 				luasnip = '[SNIP]'
 			}
 		}
-	},
-	enabled = function()
-		-- disable completion in comments
-		local context = require'cmp.config.context'
-		-- keep command mode completion enabled when cursor is in a comment
-		-- if vim.api.nvim_get_mode().mode == 'c' then
-		-- 	return true
-		-- else
-		-- 	return not context.in_treesitter_capture('comment')
-		-- 	and not context.in_syntax_group('Comment')
-		-- end
-	end
+	}
 })
 
 -- Setup lspconfig.

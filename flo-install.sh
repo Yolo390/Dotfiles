54590d7499f543ada341e59fac6979b5300c71e3
Flo Slv <florent3969@hotmail.fr>
2022-12-21 18:20:50 +0100

    🟪 refactor: add debug for install script

--- flo-install.sh
+++ flo-install.sh
@@ -2,9 +2,11 @@
 
 Flo=~/Flo
 Apps=~/Flo/Apps
+Nvim=~/.config/nvim
 
 # Coucou=~/Coucou
 # CoucouApps=~/Coucou/Apps
+# CoucouNvim=~/.config/Coucou/nvim
 
 # if [ -d "$Coucou" ] && [ -d "$CoucouApps" ]
 if [ -d "$Flo" ] && [ -d "$Apps" ]
@@ -28,9 +30,15 @@ else
 	mkdir ~/Flo/Apps
 	echo '=== ~/Flo/Apps created ! ==='
 	echo "\n"
+	echo '=== apt update ==='
+	echo "\n"
 	sudo apt update
+	echo '=== apt upgrade ==='
+	echo "\n"
 	sudo apt upgrade -y
-	# Globals dependencies
+	# Global dependencies
+	echo '=== install global dependencies ==='
+	echo "\n"
 	sudo apt install -y ripgrep fd-find silversearcher-ag bat \
 	mlocate zoxide python3-pip libsqlite3-dev libssl-dev
 	# Neovim
@@ -39,14 +47,24 @@ else
 	echo "    INSTALLING NEOVIM    "
 	echo "========================="
 	echo "\n"
+	echo '=== dependencies ==='
+	echo "\n"
 	sudo apt install -y ninja-build gettext libtool libtool-bin autoconf automake \
 	cmake g++ pkg-config doxygen
+	echo '=== remove existing vim ==='
+	echo "\n"
 	sudo apt remove -y vim
+	echo '=== git clone ==='
+	echo "\n"
 	# git clone -b release-0.8 https://github.com/neovim/neovim ~/Coucou/Apps/Neovim
 	git clone -b release-0.8 https://github.com/neovim/neovim ~/Flo/Apps/Neovim
 	# cd ~/Coucou/Apps/Neovim
 	cd ~/Flo/Apps/Neovim
+	echo '=== make ==='
+	echo "\n"
 	make CMAKE_BUILD_TYPE=RelWithDebInfo
+	echo '=== make install ==='
+	echo "\n"
 	sudo make install
 	cd ~
 	echo "\n"
@@ -84,10 +102,34 @@ else
 	echo "    INSTALLING TMUX    "
 	echo "======================="
 	echo "\n"
+	echo '=== dependencies ==='
+	echo "\n"
 	sudo apt install -y libevent-dev ncurses-dev build-essential bison
+	echo '=== remove existing tmux ==='
+	echo "\n"
 	sudo apt remove -y tmux
+	echo '=== autoremove ==='
+	echo "\n"
 	sudo apt autoremove -y
+	echo '=== remove existing .tmux ==='
+	echo "\n"
 	rm -rf .tmux
+	echo '=== git clone ==='
+	echo "\n"
+	git clone https://github.com/tmux/tmux.git ~/Flo/Apps/Tmux
+	cd ~/Flo/Apps/Tmux
+	echo '=== autogen ==='
+	echo "\n"
+	sh autogen.sh
+	echo '=== configure ==='
+	echo "\n"
+	./configure
+	echo '=== make ==='
+	echo "\n"
+	make
+	echo '=== make install ==='
+	echo "\n"
+	sudo make install
 	echo "\n"
 	echo "========================"
 	echo "    TMUX INSTALLED !    "

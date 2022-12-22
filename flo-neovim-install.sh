9aed5f18a2b31ebb682fe3fe8b6b506b482075a1
Flo Slv <florent3969@hotmail.fr>
2022-12-22 12:09:51 +0100

    🟪 refactor: typo fix in install scripts

--- flo-neovim-install.sh
+++ flo-neovim-install.sh
@@ -20,7 +20,7 @@ if [ -d "$Flo" ] && [ -d "$Apps" ]; then
 	# echo " ========================================================="
 	# echo '   Directories ~/Coucou & ~/Coucou/Apps already exists ! '
 	# echo " ========================================================="
-	echo "\n"
+	echo '\n'
 	echo " ==================================================="
 	echo '   Directories ~/Flo & ~/Flo/Apps already exists ! '
 	echo " ==================================================="
@@ -32,99 +32,99 @@ else
 	# echo '=== ~/Coucou/Apps created ! ==='
 	mkdir ~/Flo
 	echo '=== ~/Flo created ! ==='
-	echo "\n"
+	echo '\n'
 	mkdir ~/Flo/Apps
 	echo '=== ~/Flo/Apps created ! ==='
-	echo "\n"
+	echo '\n'
 	echo '=== apt update ==='
-	echo "\n"
+	echo '\n'
 	sudo apt update
 	echo '=== apt upgrade ==='
-	echo "\n"
+	echo '\n'
 	sudo apt upgrade -y
 	# Global dependencies
 	echo '=== install global dependencies ==='
-	echo "\n"
+	echo '\n'
 	sudo apt install -y ripgrep fd-find silversearcher-ag bat \
 	mlocate zoxide python3-pip libsqlite3-dev libssl-dev
 	# Neovim
-	echo "\n"
+	echo '\n'
 	echo "========================="
 	echo "    INSTALLING NEOVIM    "
 	echo "========================="
-	echo "\n"
+	echo '\n'
 	echo '=== dependencies ==='
-	echo "\n"
+	echo '\n'
 	sudo apt install -y ninja-build gettext libtool libtool-bin autoconf automake \
 	cmake g++ pkg-config doxygen
 	echo '=== remove existing vim ==='
-	echo "\n"
+	echo '\n'
 	sudo apt remove -y vim
 	echo '=== git clone release-0.8 ==='
-	echo "\n"
+	echo '\n'
 	# git clone -b release-0.8 https://github.com/neovim/neovim ~/Coucou/Apps/Neovim
 	git clone -b release-0.8 https://github.com/neovim/neovim ~/Flo/Apps/Neovim
 	# cd ~/Coucou/Apps/Neovim
 	cd ~/Flo/Apps/Neovim
 	echo '=== make ==='
-	echo "\n"
+	echo '\n'
 	make CMAKE_BUILD_TYPE=RelWithDebInfo
 	echo '=== make install ==='
-	echo "\n"
+	echo '\n'
 	sudo make install
 	cd ~
-	echo "\n"
+	echo '\n'
 	echo "=========================="
 	echo "    NEOVIM INSTALLED !    "
 	echo "=========================="
-	echo "\n"
+	echo '\n'
 	# Pynvim
-	echo "\n"
+	echo '\n'
 	echo "========================="
 	echo "    INSTALLING PYNVIM    "
 	echo "========================="
-	echo "\n"
+	echo '\n'
 	pip3 install pynvim
-	echo "\n"
+	echo '\n'
 	echo "=========================="
 	echo "    PYNVIM INSTALLED !    "
 	echo "=========================="
-	echo "\n"
+	echo '\n'
 	# Packer
-	echo "\n"
+	echo '\n'
 	echo "========================="
 	echo "    INSTALLING PACKER    "
 	echo "========================="
-	echo "\n"
+	echo '\n'
 	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-	echo "\n"
+	echo '\n'
 	echo "=========================="
 	echo "    PACKER INSTALLED !    "
 	echo "=========================="
-	echo "\n"
+	echo '\n'
 	# Neovim config
-	echo "\n"
+	echo '\n'
 	echo "==================="
 	echo "   NEOVIM CONFIG   "
 	echo "==================="
-	echo "\n"
+	echo '\n'
 	echo '=== create folder structure ==='
-	echo "\n"
+	echo '\n'
 	cd ~/.config
 	mkdir -p nvim/lua nvim/after/plugin nvim/after/ftplugin nvim/lua/FloSlv/undodir
 	echo '=== install config files ==='
-	echo "\n"
+	echo '\n'
 	wget -P ~/.config/nvim/lua/FloSlv -O options.lua https://raw.githubusercontent.com/Flo-Slv/Dotfiles/main/neovim/lua/FloSlv/options.lua
 	wget -P ~/.config/nvim/lua/FloSlv -O keymaps.lua https://raw.githubusercontent.com/Flo-Slv/Dotfiles/main/neovim/lua/FloSlv/keymaps.lua
 	wget -P ~/.config/nvim/lua/FloSlv -O utils.lua https://raw.githubusercontent.com/Flo-Slv/Dotfiles/main/neovim/lua/FloSlv/utils.lua
 	wget -P ~/.config/nvim/lua/FloSlv  -O packer.lua https://raw.githubusercontent.com/Flo-Slv/Dotfiles/main/neovim/lua/FloSlv/packer.lua
 	wget -P ~/.config/nvim -O init.lua https://raw.githubusercontent.com/Flo-Slv/Dotfiles/main/neovim/init.lua
-	echo "\n"
+	echo '\n'
 	echo "============================="
 	echo "   NEOVIM CONFIG INSTALLED   "
 	echo "============================="
-	echo "\n"
+	echo '\n'
 	echo '=== neovim version ==='
-	echo "\n"
+	echo '\n'
 	nvim --version
 fi
--- flo-tmux-install.sh
+++ flo-tmux-install.sh
@@ -18,7 +18,7 @@ if [ -d "$Flo" ] && [ -d "$Apps" ]; then
 	# echo " ========================================================="
 	# echo '   Directories ~/Coucou & ~/Coucou/Apps already exists ! '
 	# echo " ========================================================="
-	echo "\n"
+	echo '\n'
 	echo " ==================================================="
 	echo '   Directories ~/Flo & ~/Flo/Apps already exists ! '
 	echo " ==================================================="
@@ -30,61 +30,61 @@ else
 	# echo '=== ~/Coucou/Apps created ! ==='
 	mkdir ~/Flo
 	echo '=== ~/Flo created ! ==='
-	echo "\n"
+	echo '\n'
 	mkdir ~/Flo/Apps
 	echo '=== ~/Flo/Apps created ! ==='
-	echo "\n"
+	echo '\n'
 	echo '=== apt update ==='
-	echo "\n"
+	echo '\n'
 	sudo apt update
 	echo '=== apt upgrade ==='
-	echo "\n"
+	echo '\n'
 	sudo apt upgrade -y
 	# Global dependencies
 	echo '=== install global dependencies ==='
-	echo "\n"
+	echo '\n'
 	sudo apt install -y ripgrep fd-find silversearcher-ag bat \
 	mlocate zoxide python3-pip libsqlite3-dev libssl-dev
 	# Tmux
-	echo "\n"
+	echo '\n'
 	echo "======================="
 	echo "    INSTALLING TMUX    "
 	echo "======================="
-	echo "\n"
+	echo '\n'
 	echo '=== dependencies ==='
-	echo "\n"
+	echo '\n'
 	sudo apt install -y libevent-dev ncurses-dev build-essential bison
 	echo '=== remove existing tmux ==='
-	echo "\n"
+	echo '\n'
 	sudo apt remove -y tmux
 	echo '=== autoremove ==='
-	echo "\n"
+	echo '\n'
 	sudo apt autoremove -y
 	echo '=== remove existing .tmux ==='
-	echo "\n"
+	echo '\n'
 	rm -rf .tmux
 	echo '=== git clone ==='
-	echo "\n"
+	echo '\n'
 	git clone https://github.com/tmux/tmux.git ~/Flo/Apps/Tmux
 	cd ~/Flo/Apps/Tmux
 	echo '=== autogen ==='
-	echo "\n"
+	echo '\n'
 	sh autogen.sh
 	echo '=== configure ==='
-	echo "\n"
+	echo '\n'
 	./configure
 	echo '=== make ==='
-	echo "\n"
+	echo '\n'
 	make
 	echo '=== make install ==='
-	echo "\n"
+	echo '\n'
 	sudo make install
 	echo '=== tmux version ==='
-	echo "\n"
+	echo '\n'
 	tmux -V
-	echo "\n"
+	echo '\n'
 	echo "========================"
 	echo "    TMUX INSTALLED !    "
 	echo "========================"
-	echo "\n"
+	echo '\n'
 fi

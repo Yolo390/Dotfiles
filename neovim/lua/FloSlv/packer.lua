e0d0d3d49a1495f49ff6fa7981d4f333a14d8b73
Flo Slv <florent3969@hotmail.fr>
2022-12-23 12:19:20 +0100

    🟦 feat: changes on packer.lua due to stylua formatter ! :)

--- neovim/lua/FloSlv/packer.lua
+++ neovim/lua/FloSlv/packer.lua
@@ -1,179 +1,179 @@
 -- Automatically source and re-sync packer whenever you save.
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-	command = 'source <afile> | PackerSync',
+local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+	command = "source <afile> | PackerSync",
 	group = packer_group,
-	pattern = vim.fn.expand 'packer.lua'
+	pattern = vim.fn.expand("packer.lua"),
 })
 
 -- Plugins
-return require'packer'.startup({
+return require("packer").startup({
 	function(use)
 		-- Packer manager
-		use 'wbthomason/packer.nvim'
+		use("wbthomason/packer.nvim")
 
 		-- Colorscheme
-		use 'folke/tokyonight.nvim'
+		use("folke/tokyonight.nvim")
 		-- use { 'catppuccin/nvim', as = 'catppuccin' }
 
 		-- Display
-		use 'glepnir/dashboard-nvim'
-		use 'kyazdani42/nvim-web-devicons'
-		use {
-			'kyazdani42/nvim-tree.lua',
-			requires = { 'kyazdani42/nvim-web-devicons' },
-			tag = 'nightly'
-		}
-		use 'TaDaa/vimade'
-		use 'windwp/nvim-autopairs'
-		use 'norcalli/nvim-colorizer.lua'
-		use 'lukas-reineke/indent-blankline.nvim'
+		use("glepnir/dashboard-nvim")
+		use("kyazdani42/nvim-web-devicons")
+		use({
+			"kyazdani42/nvim-tree.lua",
+			requires = { "kyazdani42/nvim-web-devicons" },
+			tag = "nightly",
+		})
+		use("TaDaa/vimade")
+		use("windwp/nvim-autopairs")
+		use("norcalli/nvim-colorizer.lua")
+		use("lukas-reineke/indent-blankline.nvim")
 
 		-- which-key => easy way to find your <leader> mapping
-		use 'folke/which-key.nvim'
+		use("folke/which-key.nvim")
 
 		-- lualine => status bar (bottom)
-		use 'nvim-lualine/lualine.nvim'
+		use("nvim-lualine/lualine.nvim")
 
 		-- winbar => window bar (top)
-		use 'fgheng/winbar.nvim'
+		use("fgheng/winbar.nvim")
 
 		-- colorful-winsep => window color separation
-		use 'nvim-zh/colorful-winsep.nvim'
+		-- use 'nvim-zh/colorful-winsep.nvim'
 		-- Have an issue with Lualine and performance...
 		-- see https://github.com/nvim-zh/colorful-winsep.nvim/issues/26
 
 		-- wilder => wildmenu
-		use { 'gelguy/wilder.nvim', requires = { 'romgrk/fzy-lua-native' } }
+		use({ "gelguy/wilder.nvim", requires = { "romgrk/fzy-lua-native" } })
 
 		-- telescope => fuzzy finder
-		use {
-			'nvim-telescope/telescope.nvim',
-			branch = '0.1.x', -- tag = '0.1.0',
-			requires = { 'nvim-lua/plenary.nvim' }
-		}
+		use({
+			"nvim-telescope/telescope.nvim",
+			branch = "0.1.x", -- tag = '0.1.0',
+			requires = { "nvim-lua/plenary.nvim" },
+		})
 
 		-- telescope-fzf-native => Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-		use {
-			'nvim-telescope/telescope-fzf-native.nvim',
-			run = 'make',
+		use({
+			"nvim-telescope/telescope-fzf-native.nvim",
+			run = "make",
 			-- run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
-			cond = vim.fn.executable 'make' == 1
-		}
+			cond = vim.fn.executable("make") == 1,
+		})
 
 		-- telescope => more Telescope plugins integration.
-		use 'benfowler/telescope-luasnip.nvim'
-		use 'jvgrootveld/telescope-zoxide'
-		use 'cljoly/telescope-repo.nvim'
-		use 'AckslD/nvim-neoclip.lua'
-		use 'nvim-telescope/telescope-symbols.nvim'
-		use 'sudormrfbin/cheatsheet.nvim'
-		use 'nvim-telescope/telescope-packer.nvim'
-		use {
-			'dhruvmanila/telescope-bookmarks.nvim',
+		use("benfowler/telescope-luasnip.nvim")
+		use("jvgrootveld/telescope-zoxide")
+		use("cljoly/telescope-repo.nvim")
+		use("AckslD/nvim-neoclip.lua")
+		use("nvim-telescope/telescope-symbols.nvim")
+		use("sudormrfbin/cheatsheet.nvim")
+		use("nvim-telescope/telescope-packer.nvim")
+		use({
+			"dhruvmanila/telescope-bookmarks.nvim",
 			-- Uncomment if the selected browser is Firefox or buku
 			requires = {
-				'kkharji/sqlite.lua'
-			}
-		}
+				"kkharji/sqlite.lua",
+			},
+		})
 
 		-- harpoon => navigate between files
-		use { 'ThePrimeagen/harpoon', requires = { 'nvim-lua/plenary.nvim' } }
+		use({ "ThePrimeagen/harpoon", requires = { "nvim-lua/plenary.nvim" } })
 
 		-- nvim-treesitter => highlighting and indenting code
-	    use {
-			'nvim-treesitter/nvim-treesitter',
+		use({
+			"nvim-treesitter/nvim-treesitter",
 			run = function()
-				pcall(require'nvim-treesitter.install'.update { with_sync = true })
-			end
-		}
-		use 'p00f/nvim-ts-rainbow'
+				pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+			end,
+		})
+		use("p00f/nvim-ts-rainbow")
 
 		-- nvim-lspconfig => integration for LSP (Language Server Protocol)
 		-- mason => installing and managing LSP servers, linters and formatters
 		-- mason-lspconfig => fill the gap between nvim-lspconfig and mason
-		use {
-			'neovim/nvim-lspconfig',
+		use({
+			"neovim/nvim-lspconfig",
 			requires = {
-				'williamboman/mason.nvim',
-				'williamboman/mason-lspconfig.nvim',
+				"williamboman/mason.nvim",
+				"williamboman/mason-lspconfig.nvim",
 
 				-- Display useful loading status updates for LSP
-				'j-hui/fidget.nvim',
+				"j-hui/fidget.nvim",
 
 				-- Additional lua configuration, makes nvim stuff amazing
-				'folke/neodev.nvim',
+				"folke/neodev.nvim",
 
-				'RRethy/vim-illuminate'
-			}
-		}
+				"RRethy/vim-illuminate",
+			},
+		})
 
 		-- null-ls => formatting and linting
 		-- mason-null-ls => fill the gap between null-ls and mason
-		use 'jose-elias-alvarez/null-ls.nvim'
-		use 'jayp0521/mason-null-ls.nvim'
+		use("jose-elias-alvarez/null-ls.nvim")
+		use("jayp0521/mason-null-ls.nvim")
 
 		-- nvim-cmp => autocompletion
-		use {
-			'hrsh7th/nvim-cmp',
+		use({
+			"hrsh7th/nvim-cmp",
 			requires = {
-				'hrsh7th/cmp-nvim-lsp', -- integration for nvim-lspconfig
-				'hrsh7th/cmp-path',
-				'hrsh7th/cmp-buffer',
-				'hrsh7th/cmp-cmdline',
-				'L3MON4D3/LuaSnip',
-				'saadparwaiz1/cmp_luasnip', -- integration for luasnip
-				'rafamadriz/friendly-snippets', -- collection of snippets
-				'onsails/lspkind.nvim'
-			}
-		}
+				"hrsh7th/cmp-nvim-lsp", -- integration for nvim-lspconfig
+				"hrsh7th/cmp-path",
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-cmdline",
+				"L3MON4D3/LuaSnip",
+				"saadparwaiz1/cmp_luasnip", -- integration for luasnip
+				"rafamadriz/friendly-snippets", -- collection of snippets
+				"onsails/lspkind.nvim",
+			},
+		})
 
 		-- Databases
-		use 'tpope/vim-dadbod'
-		use 'kristijanhusak/vim-dadbod-ui'
+		use("tpope/vim-dadbod")
+		use("kristijanhusak/vim-dadbod-ui")
 
 		-- Comment => easy way to comment code
-		use 'numToStr/Comment.nvim'
+		use("numToStr/Comment.nvim")
 
 		-- nvim-notify => notifications
-		use 'rcarriga/nvim-notify'
+		use("rcarriga/nvim-notify")
 
 		-- GIT
-		use 'lewis6991/gitsigns.nvim'
+		use("lewis6991/gitsigns.nvim")
 
 		-- glow => markdown
-		use 'ellisonleao/glow.nvim'
+		use("ellisonleao/glow.nvim")
 
 		-- Documentation
 		-- check if it's still relevant since we have neodev for lsp !
-		use 'nanotee/luv-vimdocs'
-		use 'milisims/nvim-luaref'
+		use("nanotee/luv-vimdocs")
+		use("milisims/nvim-luaref")
 
 		-- rust-tools => toolbox for Rust
-		use 'simrat39/rust-tools.nvim'
+		use("simrat39/rust-tools.nvim")
 
 		-- nvim-dap => debug
-		use 'mfussenegger/nvim-dap'
+		use("mfussenegger/nvim-dap")
 
 		-- HTML
-		-- use 'AndrewRadev/tagalong.vim' -- Change an HTML(ish) opening/closing tag 
-		use 'tpope/vim-surround'
-		use 'windwp/nvim-ts-autotag'
+		-- use 'AndrewRadev/tagalong.vim' -- Change an HTML(ish) opening/closing tag
+		use("tpope/vim-surround")
+		use("windwp/nvim-ts-autotag")
 
 		-- taboo => tab manager
-		use 'gcmt/taboo.vim'
+		use("gcmt/taboo.vim")
 
 		-- vim-signature => easy way to manage marks
-		use 'kshenoy/vim-signature'
+		use("kshenoy/vim-signature")
 
 		-- vim-tmux-navigator => integration for Tmux !
-		use 'christoomey/vim-tmux-navigator'
+		use("christoomey/vim-tmux-navigator")
 	end,
 	config = {
 		display = {
 			open_fn = function()
-				return require('packer.util').float({ border = 'single' })
-			end
-		}
-	}
+				return require("packer.util").float({ border = "single" })
+			end,
+		},
+	},
 })

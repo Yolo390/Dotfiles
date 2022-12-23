869a5f698115f245ce5be168f604a88eba9a1cc3
Flo Slv <florent3969@hotmail.fr>
2022-12-23 14:34:05 +0100

    🟦 feat: stylua telescope config file + new mapping to move

--- neovim/after/plugin/telescope.lua
+++ neovim/after/plugin/telescope.lua
@@ -1,49 +1,62 @@
-local ts_status, telescope = pcall(require, 'telescope')
-if not ts_status then
+local ts_ok, telescope = pcall(require, "telescope")
+if not ts_ok then
 	return
 end
 
-telescope.setup {
+local ts_action_ok, ts_actions = pcall(require, "telescope.actions")
+if not ts_action_ok then
+	return
+end
+
+telescope.setup({
 	defaults = {
-		prompt_prefix = '🔍 ',
+		prompt_prefix = "🔍 ",
 		hidden = true,
 		mappings = {
-			-- Delete a buffer with ctrl+d when opening buffers list ('fb' command)
-			n = { ['<C-d>'] = require'telescope.actions'.delete_buffer }
-		}
+			n = {
+				["<C-j>"] = ts_actions.move_selection_next,
+				["<C-k>"] = ts_actions.move_selection_previous,
+				["<C-d>"] = ts_actions.delete_buffer,
+			},
+			i = {
+				["<C-j>"] = ts_actions.move_selection_next,
+				["<C-k>"] = ts_actions.move_selection_previous,
+				["<C-d>"] = ts_actions.delete_buffer,
+			},
+		},
 	},
 	extensions = {
 		bookmarks = {
-			selected_browser = 'firefox',
-			url_open_command = 'open', -- Either provide a shell command to open the URL
+			selected_browser = "firefox",
+			url_open_command = "open", -- Either provide a shell command to open the URL
 			url_open_plugin = nil, -- Available: 'vim_external', 'open_browser'
 			full_path = true,
 			firefox_profile_name = nil,
-			debug = false
-		}
-	}
-}
+			debug = false,
+		},
+	},
+})
 
-pcall(telescope.load_extension, 'fzf')
-pcall(telescope.load_extension, 'luasnip')
-pcall(telescope.load_extension, 'notify')
-pcall(telescope.load_extension, 'zoxide')
-pcall(telescope.load_extension, 'repo')
-pcall(telescope.load_extension, 'packer')
-pcall(telescope.load_extension, 'bookmarks')
-pcall(telescope.load_extension, 'neoclip')
-require'neoclip'.setup()
+pcall(telescope.load_extension, "fzf")
+pcall(telescope.load_extension, "luasnip")
+pcall(telescope.load_extension, "notify")
+pcall(telescope.load_extension, "zoxide")
+pcall(telescope.load_extension, "repo")
+pcall(telescope.load_extension, "packer")
+pcall(telescope.load_extension, "bookmarks")
+pcall(telescope.load_extension, "neoclip")
+require("neoclip").setup()
 
 -- UI
-vim.api.nvim_set_hl(0, 'TelescopeBorder', { fg='#CCCCCC' })
-vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg='#CCCCCC' })
-vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { fg='#CCCCCC' })
-vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { fg='#CCCCCC' })
+vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#CCCCCC" })
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#CCCCCC" })
+vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#CCCCCC" })
+vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#CCCCCC" })
 
-vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { bg='#24283b' })
-vim.api.nvim_set_hl(0, 'TelescopeResultsNormal', { bg='#24283b' })
-vim.api.nvim_set_hl(0, 'TelescopePreviewNormal', { bg='#24283b' })
+vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "#24283b" })
+vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "#24283b" })
+vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "#24283b" })
 
-vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { fg='#ffffff' })
-vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { fg='#ffffff' })
-vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { fg='#ffffff' })
+vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = "#ffffff" })

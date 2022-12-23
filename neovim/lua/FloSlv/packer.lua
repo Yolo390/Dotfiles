2a8a42e5684918ec2265a0a164c91993395d60f9
Flo Slv <florent3969@hotmail.fr>
2022-12-23 13:20:33 +0100

    🟦 feat: packer ensure installed function + pcall when require

--- neovim/lua/FloSlv/packer.lua
+++ neovim/lua/FloSlv/packer.lua
@@ -1,3 +1,16 @@
+-- auto install packer if not installed
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
 -- Automatically source and re-sync packer whenever you save.
 local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePost", {
@@ -5,6 +18,11 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 	group = packer_group,
 	pattern = vim.fn.expand("packer.lua"),
 })
+
+local status, packer = pcall(require, "packer")
+if not status then
+	return
+end
 
 -- Plugins
 return require("packer").startup({

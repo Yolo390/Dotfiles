d708044f736469852521a2034b229b6d2d5332b5
Flo Slv <florent3969@hotmail.fr>
2022-12-23 12:48:25 +0100

    🟦 feat: null-ls plugin config stylua + run eslint only if confi
    config file is present !

--- neovim/after/plugin/null-ls.lua
+++ neovim/after/plugin/null-ls.lua
@@ -1,4 +1,4 @@
-local null_ls_status, null_ls = pcall(require, 'null-ls')
+local null_ls_status, null_ls = pcall(require, "null-ls")
 if not null_ls_status then
 	return
 end
@@ -6,32 +6,37 @@ end
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
-local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+-- To setup format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
 	sources = {
-		formatting.prettier,
-		formatting.stylua,
-		diagnostics.eslint_d
+		formatting.prettier, -- JS/TS formatter
+		formatting.stylua, -- lua formatter
+		diagnostics.eslint_d.with({ -- JS/TS linter
+			-- only enable eslint if root has .eslintrc.js
+			condition = function(utils)
+				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
+			end,
+		}),
 	},
 	-- configure format on save
-	on_attach = function (current_client, bufnr)
-		if current_client.supports_method('textDocument/formatting') then
+	on_attach = function(current_client, bufnr)
+		if current_client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd('BufWritePre', {
+			vim.api.nvim_create_autocmd("BufWritePre", {
 				group = augroup,
 				buffer = bufnr,
-				callback = function ()
+				callback = function()
 					vim.lsp.buf.format({
-						filter = function (client)
+						filter = function(client)
 							-- only use null-ls for formatting instead of lsp-server
-							return client.name == 'null-ls'
+							return client.name == "null-ls"
 						end,
-						bufnr = bufnr
+						bufnr = bufnr,
 					})
-				end
+				end,
 			})
 		end
-	end
+	end,
 })
-

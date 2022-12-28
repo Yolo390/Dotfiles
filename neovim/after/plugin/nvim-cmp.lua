ab998d46bc3363f9011dad4117effbedbdc9bd3f
Flo Slv <florent3969@hotmail.fr>
2022-12-28 23:05:31 +0100

    🟪 refactor: nvim-cmp prettier

--- neovim/after/plugin/nvim-cmp.lua
+++ neovim/after/plugin/nvim-cmp.lua
@@ -1,40 +1,40 @@
-local cmp_status, cmp = pcall(require, 'cmp')
-if not cmp_status then
+local cmp_ok, cmp = pcall(require, "cmp")
+if not cmp_ok then
 	return
 end
 
-local lspkind_status, lspkind = pcall(require, 'lspkind')
-if not lspkind_status then
+local lspkind_ok, lspkind = pcall(require, "lspkind")
+if not lspkind_ok then
 	return
 end
 
-local luasnip_status, luasnip = pcall(require, 'luasnip')
-if not luasnip_status then
+local luasnip_ok, luasnip = pcall(require, "luasnip")
+if not luasnip_ok then
 	return
 end
 
-local luasnip_vsc_status, luasnip_load_vsc = pcall(require, 'luasnip.loaders.from_vscode')
-if not luasnip_vsc_status then
+local luasnip_vsc_ok, luasnip_load_vsc = pcall(require, "luasnip.loaders.from_vscode")
+if not luasnip_vsc_ok then
 	return
 end
 
 luasnip_load_vsc.lazy_load()
 
-cmp.setup {
+cmp.setup({
 	snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body)
-		end
+		end,
 	},
-	mapping = cmp.mapping.preset.insert {
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<CR>'] = cmp.mapping.confirm {
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
-			select = true
-		},
-		['<C-j>'] = cmp.mapping(function(fallback)
+			select = true,
+		}),
+		["<C-j>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expand_or_jumpable() then
@@ -42,8 +42,8 @@ cmp.setup {
 			else
 				fallback()
 			end
-		end, { 'i', 's' }),
-		['<C-k>'] = cmp.mapping(function(fallback)
+		end, { "i", "s" }),
+		["<C-k>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
@@ -51,28 +51,33 @@ cmp.setup {
 			else
 				fallback()
 			end
-		end, { 'i', 's' }),
-	},
+		end, { "i", "s" }),
+	}),
 	sources = {
-		{ name = 'path' },
-		{ name = 'nvim_lsp', keyword_length = 3 },
-		{ name = 'luasnip' },
-		{ name = 'buffer', keyword_length = 3 }
+		{ name = "nvim_lsp", keyword_length = 2 },
+		{ name = "path" },
+		{ name = "luasnip", keyword_length = 3 },
+		{ name = "buffer", keyword_length = 3 },
 	},
 	window = {
 		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered()
+		documentation = cmp.config.window.bordered(),
 	},
 	formatting = {
+		fields = {
+			cmp.ItemField.Menu,
+			cmp.ItemField.Abbr,
+			cmp.ItemField.Kind,
+		},
 		format = lspkind.cmp_format({
-			mode = 'symbol_text',
-			menu = ({
-				buffer = '[BUF]',
-				nvim_lsp = '[LSP]',
-				nvim_lua = '[LUA]',
-				path = '[PATH]',
-				luasnip = '[SNIP]'
-			})
-		})
-	}
-}
+			mode = "symbol_text",
+			menu = {
+				buffer = "[BUF]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[LUA]",
+				path = "[PATH]",
+				luasnip = "[SNIP]",
+			},
+		}),
+	},
+})

d897dc2ca330b1ea64bc37dc6fb282ad46c63c05
Flo Slv <florent3969@hotmail.fr>
2023-01-10 18:06:47 +0100

    🟪 refactor: prettier on wilder

--- neovim/after/plugin/wilder.lua
+++ neovim/after/plugin/wilder.lua
@@ -1,86 +1,97 @@
-local ok, wilder = pcall(require, 'wilder')
+local ok, wilder = pcall(require, "wilder")
 if not ok then
 	return
 end
 
 wilder.setup({
-	modes = { ':', '/', '?' },
-    next_key = '<C-j>',
-    previous_key = '<C-k>'
+	modes = { ":", "/", "?" },
+	next_key = "<C-j>",
+	previous_key = "<C-k>",
 })
 
-wilder.set_option('pipeline', {
+wilder.set_option("pipeline", {
 	wilder.branch(
 		wilder.python_file_finder_pipeline({
 			file_command = function(_, arg)
-				if string.find(arg, '.') ~= nil then
-					return {'fdfind', '-tf', '-H'}
+				if string.find(arg, ".") ~= nil then
+					return { "fdfind", "-tf", "-H" }
 				else
-					return {'fdfind', '-tf'}
+					return { "fdfind", "-tf" }
 				end
 			end,
-			dir_command = {'fd', '-td'},
-			filters = {'fuzzy_filter', 'difflib_sorter'}
+			dir_command = { "fd", "-td" },
+			filters = { "fuzzy_filter", "difflib_sorter" },
 		}),
 		wilder.substitute_pipeline({
 			pipeline = wilder.python_search_pipeline({
 				skip_cmdtype_check = 1,
-				pattern = wilder.python_fuzzy_pattern({ start_at_boundary = 0 })
-			})
+				pattern = wilder.python_fuzzy_pattern({ start_at_boundary = 0 }),
+			}),
 		}),
 		wilder.cmdline_pipeline({
 			fuzzy = 1,
-			fuzzy_filter = wilder.lua_fzy_filter()
+			fuzzy_filter = wilder.lua_fzy_filter(),
 		}),
 		{
-			wilder.check(function(_, x) return x == '' end),
-			wilder.history()
+			wilder.check(function(_, x)
+				return x == ""
+			end),
+			wilder.history(),
 		},
 		wilder.python_search_pipeline({
-			pattern = wilder.python_fuzzy_pattern({ start_at_boundary = 0 })
+			pattern = wilder.python_fuzzy_pattern({ start_at_boundary = 0 }),
 		})
-	)
+	),
 })
 
 local gradient = {
-  '#f4468f', '#fd4a85', '#ff507a', '#ff566f', '#ff5e63',
-  '#ff6658', '#ff704e', '#ff7a45', '#ff843d', '#ff9036',
-  '#f89b31', '#efa72f', '#e6b32e', '#dcbe30', '#d2c934',
-  '#c8d43a', '#bfde43', '#b6e84e', '#aff05b'
+	"#f4468f",
+	"#fd4a85",
+	"#ff507a",
+	"#ff566f",
+	"#ff5e63",
+	"#ff6658",
+	"#ff704e",
+	"#ff7a45",
+	"#ff843d",
+	"#ff9036",
+	"#f89b31",
+	"#efa72f",
+	"#e6b32e",
+	"#dcbe30",
+	"#d2c934",
+	"#c8d43a",
+	"#bfde43",
+	"#b6e84e",
+	"#aff05b",
 }
 
 for i, fg in ipairs(gradient) do
-	gradient[i] = wilder.make_hl(
-		'WilderGradient' .. i,
-		'Pmenu',
-		{
-			{a = 1},
-			{a = 1},
-			{foreground = fg}
-		}
-	)
+	gradient[i] = wilder.make_hl("WilderGradient" .. i, "Pmenu", {
+		{ a = 1 },
+		{ a = 1 },
+		{ foreground = fg },
+	})
 end
 
-local popupmenu_renderer = wilder.popupmenu_renderer(
-	wilder.popupmenu_border_theme({
-		border = 'rounded',
-		empty_message = wilder.popupmenu_empty_message_with_spinner(),
-		highlights = { gradient = gradient }, -- must be set
-		highlighter = wilder.highlighter_with_gradient({
-			-- wilder.basic_highlighter(),
-			wilder.lua_fzy_highlighter(),
+local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+	border = "rounded",
+	empty_message = wilder.popupmenu_empty_message_with_spinner(),
+	highlights = { gradient = gradient }, -- must be set
+	highlighter = wilder.highlighter_with_gradient({
+		-- wilder.basic_highlighter(),
+		wilder.lua_fzy_highlighter(),
+	}),
+	left = {
+		" ",
+		wilder.popupmenu_devicons(),
+		wilder.popupmenu_buffer_flags({
+			flags = " a + ",
+			icons = { ["+"] = "", a = "", h = "" },
 		}),
-		left = {
-			' ',
-			wilder.popupmenu_devicons(),
-			wilder.popupmenu_buffer_flags({
-				flags = ' a + ',
-				icons = { ['+'] = '', a = '', h = '' }
-			})
-		},
-		right = { ' ', wilder.popupmenu_scrollbar() }
-	})
-)
+	},
+	right = { " ", wilder.popupmenu_scrollbar() },
+}))
 
 local wildmenu_renderer = wilder.wildmenu_renderer({
 	highlights = { gradient = gradient }, -- must be set
@@ -88,13 +99,16 @@ local wildmenu_renderer = wilder.wildmenu_renderer({
 		-- wilder.basic_highlighter(),
 		wilder.lua_fzy_highlighter(),
 	}),
-	separator = ' · ',
-	left = {' ', wilder.wildmenu_spinner(), ' '},
-	right = {' ', wilder.wildmenu_index()}
+	separator = " · ",
+	left = { " ", wilder.wildmenu_spinner(), " " },
+	right = { " ", wilder.wildmenu_index() },
 })
 
-wilder.set_option('renderer', wilder.renderer_mux({
-	[':'] = popupmenu_renderer,
-	['/'] = wildmenu_renderer,
-	['?'] = wildmenu_renderer
-}))
+wilder.set_option(
+	"renderer",
+	wilder.renderer_mux({
+		[":"] = popupmenu_renderer,
+		["/"] = wildmenu_renderer,
+		["?"] = wildmenu_renderer,
+	})
+)

0a3b7183097cc6745efad2c98c98fdfcb1c736d0
Flo Slv <florent3969@hotmail.fr>
2023-01-11 11:16:26 +0100

    🟪 refactor: change nvim-notify config

--- neovim/after/plugin/nvim-notify.lua
+++ neovim/after/plugin/nvim-notify.lua
@@ -1,6 +1,13 @@
-local not_status, notify = pcall(require, 'notify')
+local not_status, notify = pcall(require, "notify")
 if not not_status then
 	return
 end
+
+notify.setup({
+	render = "default",
+	timeout = 500,
+	top_down = true,
+	stages = "slide",
+})
 
 vim.notify = notify

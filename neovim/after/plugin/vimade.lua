2a41107ed57a71c22d13733560e4b28089ed2ffd
Flo Slv <florent3969@hotmail.fr>
2023-01-16 19:43:14 +0100

    🟪 refactor: prettier on vimade plugin

--- neovim/after/plugin/vimade.lua
+++ neovim/after/plugin/vimade.lua
@@ -2,34 +2,33 @@ vim.g.vimade = {
 	fadelevel = 0.4,
 	enabletreesitter = 0,
 	enablesigns = 1,
-	enablefocusfading = 1
+	enablefocusfading = 1,
 }
 
-local custom_fade = vim.api.nvim_create_augroup('custom_fade', { clear = true })
+local custom_fade = vim.api.nvim_create_augroup("custom_fade", { clear = true })
 
-vim.api.nvim_create_autocmd({ 'FileType', 'WinEnter' }, {
+vim.api.nvim_create_autocmd({ "FileType", "WinEnter" }, {
 	group = custom_fade,
-	callback = function ()
+	callback = function()
 		-- Reset ColorColumn when enter Telescope popup prompt
 		local win_type = vim.fn.win_gettype(0)
 		local filetype = vim.bo.filetype
 
-		if (win_type == 'popup' and filetype == 'TelescopePrompt') then
-			vim.cmd [[ highlight ColorColumn guibg=#24283b ]]
+		if win_type == "popup" and filetype == "TelescopePrompt" then
+			vim.cmd([[ highlight ColorColumn guibg=#24283b ]])
 		end
-
-	end
+	end,
 })
 
-vim.api.nvim_create_autocmd({ 'WinLeave' }, {
+vim.api.nvim_create_autocmd({ "WinLeave" }, {
 	group = custom_fade,
-	callback = function ()
+	callback = function()
 		-- Reset ColorColumn when leave Telescope popup prompt
 		local win_type = vim.fn.win_gettype(0)
 		local filetype = vim.bo.filetype
 
-		if (win_type == 'popup' and filetype == 'TelescopePrompt') then
-			vim.cmd [[ highlight ColorColumn guibg=#1f2335 ]]
+		if win_type == "popup" and filetype == "TelescopePrompt" then
+			vim.cmd([[ highlight ColorColumn guibg=#1f2335 ]])
 		end
-	end
+	end,
 })

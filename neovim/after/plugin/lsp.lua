1c08ffd645b46438994b2f5846ed7136c9144d02
Flo Slv <florent3969@hotmail.fr>
2023-01-16 18:52:44 +0100

    🟪 refactor: LSP change info icon

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -180,7 +180,7 @@ vim.diagnostic.config({
 	},
 })
 
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })

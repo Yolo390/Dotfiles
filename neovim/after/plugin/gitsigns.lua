306f136689d69e766d19b975d45041ff83d0fcc6
Flo Slv <florent3969@hotmail.fr>
2023-01-26 13:55:29 +0100

    🟪 refactor: prettier on gitsigns config

--- neovim/after/plugin/gitsigns.lua
+++ neovim/after/plugin/gitsigns.lua
@@ -1,72 +1,72 @@
-local gs_status, gitsigns = pcall(require, 'gitsigns')
+local gs_status, gitsigns = pcall(require, "gitsigns")
 if not gs_status then
 	return
 end
 
-gitsigns.setup {
+gitsigns.setup({
 	signs = {
 		add = {
-			hl = 'GitSignsAdd',
-			text = ' │',
-			numhl='GitSignsAddNr',
-			linehl='GitSignsAddLn'
+			hl = "GitSignsAdd",
+			text = " │",
+			numhl = "GitSignsAddNr",
+			linehl = "GitSignsAddLn",
 		},
 		change = {
-			hl = 'GitSignsChange',
-			text = ' │',
-			numhl='GitSignsChangeNr',
-			linehl='GitSignsChangeLn'
+			hl = "GitSignsChange",
+			text = " │",
+			numhl = "GitSignsChangeNr",
+			linehl = "GitSignsChangeLn",
 		},
 		delete = {
-			hl = 'GitSignsDelete',
-			text = '_',
-			numhl='GitSignsDeleteNr',
-			linehl='GitSignsDeleteLn'
+			hl = "GitSignsDelete",
+			text = "_",
+			numhl = "GitSignsDeleteNr",
+			linehl = "GitSignsDeleteLn",
 		},
 		topdelete = {
-			hl = 'GitSignsDelete',
-			text = '‾',
-			numhl='GitSignsDeleteNr',
-			linehl='GitSignsDeleteLn'
+			hl = "GitSignsDelete",
+			text = "‾",
+			numhl = "GitSignsDeleteNr",
+			linehl = "GitSignsDeleteLn",
 		},
 		changedelete = {
-			hl = 'GitSignsChange',
-			text = '~',
-			numhl='GitSignsChangeNr',
-			linehl='GitSignsChangeLn'
-		}
+			hl = "GitSignsChange",
+			text = "~",
+			numhl = "GitSignsChangeNr",
+			linehl = "GitSignsChangeLn",
+		},
 	},
-	signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-	numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-	linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-	word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 	watch_gitdir = {
 		interval = 1000,
-		follow_files = true
+		follow_files = true,
 	},
 	attach_to_untracked = true,
 	current_line_blame = false,
 	current_line_blame_opts = {
 		virt_text = true,
-		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
 		delay = 500,
-		ignore_whitespace = false
+		ignore_whitespace = false,
 	},
-	current_line_blame_formatter = '<author>, <author_time:%d-%m-%Y> - <summary>',
+	current_line_blame_formatter = "<author>, <author_time:%d-%m-%Y> - <summary>",
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
 	max_file_length = 40000,
 	preview_config = {
 		-- Options passed to nvim_open_win
-		border = 'single',
-		style = 'minimal',
-		relative = 'cursor',
+		border = "single",
+		style = "minimal",
+		relative = "cursor",
 		row = 0,
-		col = 1
+		col = 1,
 	},
 	yadm = {
-		enable = false
+		enable = false,
 	},
 	on_attach = function(bufnr)
 		local gs = package.loaded.gitsigns
@@ -78,21 +78,31 @@ gitsigns.setup {
 		end
 
 		-- Navigation
-		map('n', '<leader>gn', function()
-			if vim.wo.diff then return ']c' end
-			vim.schedule(function() gs.next_hunk() end)
-			return '<Ignore>'
-		end, {expr=true})
+		map("n", "<leader>gn", function()
+			if vim.wo.diff then
+				return "]c"
+			end
+			vim.schedule(function()
+				gs.next_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
 
-		map('n', '<leader>gN', function()
-			if vim.wo.diff then return '[c' end
-			vim.schedule(function() gs.prev_hunk() end)
-			return '<Ignore>'
-		end, {expr=true})
+		map("n", "<leader>gN", function()
+			if vim.wo.diff then
+				return "[c"
+			end
+			vim.schedule(function()
+				gs.prev_hunk()
+			end)
+			return "<Ignore>"
+		end, { expr = true })
 
 		-- Actions
-		map('n', '<leader>gh', gs.preview_hunk)
-		map('n', '<leader>gg', function() gs.blame_line{ full=true } end)
-		map('n', '<leader>gt', gs.toggle_current_line_blame)
-	end
-}
+		map("n", "<leader>gh", gs.preview_hunk)
+		map("n", "<leader>gg", function()
+			gs.blame_line({ full = true })
+		end)
+		map("n", "<leader>gt", gs.toggle_current_line_blame)
+	end,
+})

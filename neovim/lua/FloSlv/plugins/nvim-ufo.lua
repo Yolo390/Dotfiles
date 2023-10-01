b2cf9cb83c485eb01b7687a180d921c1dc8e3e8e
Flo Slv <florent3969@hotmail.fr>
2023-10-01 16:54:06 +0200

    🟪 refactor: Neovim do not use vim-ufo
    I don't have time yet to explore how it's work.

---
+++ neovim/lua/FloSlv/plugins/nvim-ufo.lua
@@ -0,0 +1,13 @@
+return {}
+
+-- return {
+--   "kevinhwang91/nvim-ufo",
+--   dependencies = {
+--     "kevinhwang91/promise-async",
+--   },
+-- 	config = function ()
+-- 		local ufo = require("nvim-ufo")
+--
+-- 		ufo.setup()
+-- 	end
+-- }

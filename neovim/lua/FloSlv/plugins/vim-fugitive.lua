235a3892c984de6549f2b79de274bb03a6cbaf6f
Flo Slv <florent3969@hotmail.fr>
2023-10-01 15:48:57 +0200

    🟦 feat: Neovim: add vim-fugitive package

---
+++ neovim/lua/FloSlv/plugins/vim-fugitive.lua
@@ -0,0 +1,15 @@
+return {
+  "tpope/vim-fugitive",
+  config = function()
+    local map = function(key, cmd, opts)
+      opts = opts or {}
+      opts.noremap = true
+      opts.silent = true
+
+      vim.keymap.set("n", key, cmd, opts)
+    end
+
+    map("<leader>go", ":vertical Git<CR>", { desc = "Fugitive: git open" })
+    map("<leader>gs", ":Git status<CR>", { desc = "Fugitive: git status" })
+  end,
+}

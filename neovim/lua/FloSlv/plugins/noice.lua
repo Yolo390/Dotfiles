428d022cf2643195740f56268ccbade4d661fbaa
Flo Slv <florent.sanlaville@uha.fr>
2023-10-04 15:56:05 +0200

    🟪 refactor: Neovim improve noice.lua

--- neovim/lua/FloSlv/plugins/noice.lua
+++ neovim/lua/FloSlv/plugins/noice.lua
@@ -1,57 +1,54 @@
 return {
   {
     "folke/noice.nvim",
-		dependencies = {
-			"MunifTanjim/nui.nvim"
-		},
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
     event = "VeryLazy",
+    keys = {
+      {
+        "<leader>uu",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Dismiss all screen notifications",
+      },
+    },
     opts = {
       lsp = {
-        -- override = {
-        --   ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        --   ["vim.lsp.util.stylize_markdown"] = true,
-        --   ["cmp.entry.get_documentation"] = true,
-        -- },
-				signature = {
-					enabled = false
-				},
-				hover = {
-					enabled = false
-				}
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+        signature = {
+          enabled = false,
+        },
+        hover = {
+          enabled = false,
+        },
       },
-      -- routes = {
-      --   {
-      --     filter = {
-      --       event = "msg_show",
-      --       any = {
-      --         { find = "%d+L, %d+B" },
-      --         { find = "; after #%d+" },
-      --         { find = "; before #%d+" },
-      --       },
-      --     },
-      --     view = "mini",
-      --   },
-      -- },
       presets = {
         bottom_search = true,
         command_palette = true,
         long_message_to_split = true,
         inc_rename = true,
       },
-			views = {
-				cmdline_popup = {
-					position = {
-						row = -2,
-						col = "50%"
-					}
-				},
-				cmdline_popupmenu = {
-					position = {
-						row = -5,
-						col = "50%"
-					}
-				}
-			}
+      views = {
+        cmdline_popup = {
+          position = {
+            row = -2,
+            col = "50%",
+          },
+        },
+        cmdline_popupmenu = {
+          position = {
+            row = -5,
+            col = "50%",
+          },
+        },
+      },
     },
   },
 }

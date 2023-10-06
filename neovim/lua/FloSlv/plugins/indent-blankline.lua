e47e6b1534a167456c81f7c5babce0eddc7f75d2
Flo Slv <florent3969@hotmail.fr>
2023-10-06 11:05:52 +0200

    🟪 refactor: Neovim update indent-blankline config from 2 to 3

--- neovim/lua/FloSlv/plugins/indent-blankline.lua
+++ neovim/lua/FloSlv/plugins/indent-blankline.lua
@@ -1,25 +1,30 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-	main = "ibl",
+  main = "ibl",
   event = { "BufReadPost", "BufNewFile" },
   opts = {
-    char = "│",
-    filetype_exclude = {
-      "help",
-      "alpha",
-      "dashboard",
-      "Trouble",
-      "lazy",
-      "mason",
-      "notify",
-      "toggleterm",
-      "lazyterm",
-      "NvimTree",
+    indent = {
+      char = "│",
+      -- highlight = { "Function", "Label" },
     },
-    show_current_context = true,
-    show_current_context_start = true,
-    -- show_trailing_blankline_indent = false,
-    -- show_end_of_line = true,
-    -- space_char_blankline = '.'
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "Trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+        "NvimTree",
+      },
+    },
+    scope = {
+      enabled = true,
+      show_start = true,
+      show_end = true,
+    },
   },
 }

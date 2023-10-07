5a9b176f168d91e825a30cdd012faa29ed921da9
Flo Slv <florent3969@hotmail.fr>
2023-10-07 11:35:23 +0200

    🟪 refactor: Keymaps.lua - use utils.map

--- neovim/lua/FloSlv/core/keymaps.lua
+++ neovim/lua/FloSlv/core/keymaps.lua
@@ -2,71 +2,119 @@
 -- # KEYMAPS #
 -- ###########
 
-local keymap = vim.keymap.set
+local utils = require("FloSlv.core.utils")
 
 local full_options = { noremap = true, silent = true }
 local noremap = { noremap = true }
 
-local nmap = function(key, command, option, desc)
-  if option == "full_options" then
-    keymap("n", key, command, { desc = desc, noremap = true, silent = true })
-  else
-    keymap("n", key, command, { desc = desc, noremap = true })
-  end
-end
-
 -- SAVE AND RE SOURCE CURRENT FILE.
-nmap(
+utils.map(
+  "n",
   "<leader>s",
   ":w | source %<CR>",
-  "full_options",
-  "Save and re source current file."
+  "Save and re source current file.",
+  full_options
 )
 
 -- WINDOWS
-nmap("<leader>v", ":vsplit<CR>", "full_options", "Windows: split vertically.")
-nmap("<leader>x", ":split<CR>", "full_options", "Windows: split horizontally.")
+utils.map(
+  "n",
+  "<leader>v",
+  ":vsplit<CR>",
+  "Windows: split vertically.",
+  full_options
+)
+utils.map(
+  "n",
+  "<leader>x",
+  ":split<CR>",
+  "Windows: split horizontally.",
+  full_options
+)
 
 -- TABS.
-keymap("n", "tt", ":tabnew<CR>", full_options)
-keymap("n", "H", "gT", noremap)
-keymap("n", "L", "gt", noremap)
+utils.map("n", "tt", ":tabnew<CR>", "Tab: new", full_options)
+utils.map("n", "H", "gT", "Tab: got to previous.", noremap)
+utils.map("n", "L", "gt", "Tab: got to next.", noremap)
 
 -- MOVING TEXT.
-keymap("v", "J", ":move '>+1<CR>gv=gv", full_options)
-keymap("v", "K", ":move '<-2<CR>gv=gv", full_options)
-keymap("n", "<C-j>", ":move .+1<CR>==", full_options)
-keymap("n", "<C-k>", ":move .-2<CR>==", full_options)
+utils.map(
+  "v",
+  "J",
+  ":move '>+1<CR>gv=gv",
+  "Moving txt: one line down.",
+  full_options
+)
+utils.map(
+  "v",
+  "K",
+  ":move '<-2<CR>gv=gv",
+  "Moving txt: one line up.",
+  full_options
+)
+utils.map(
+  "n",
+  "<C-j>",
+  ":move .+1<CR>==",
+  "Moving text: block down. ",
+  full_options
+)
+utils.map(
+  "n",
+  "<C-k>",
+  ":move .-2<CR>==",
+  "Moving text: block up.",
+  full_options
+)
 
 -- REPLACE WORD UNDER CURSOR ACROSS ENTIRE BUFFER.
-keymap(
+utils.map(
   "n",
   "<C-w>",
   [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  "Replace word under cursor across entire buffer.",
   full_options
 )
 
 -- COPY FROM CURSOR TO END OF LINE.
-keymap("n", "Y", "y$", noremap)
+utils.map("n", "Y", "y$", "Copy from cursor to end of line", noremap)
 
 -- KEEPING IN CENTERED
-keymap("n", "n", "nzzzv", full_options)
-keymap("n", "N", "Nzzzv", full_options)
-keymap("n", "G", "Gzz", full_options)
+utils.map("n", "n", "nzzzv", "Keeping in centered: n", full_options)
+utils.map("n", "N", "Nzzzv", "Keeping in centered: N", full_options)
+utils.map("n", "G", "Gzz", "Keeping in centered: G", full_options)
 
 -- UNDO BREAK POINTS
-keymap("i", ",", ",<C-g>u", {})
-keymap("i", ";", ";<C-g>u", {})
-keymap("i", ".", ".<C-g>u", {})
-keymap("i", "!", "!<C-g>u", {})
-keymap("i", "?", "?<C-g>u", {})
+utils.map("i", ",", ",<C-g>u", "Undo break points: ','", {})
+utils.map("i", ";", ";<C-g>u", "Undo break points: ';'", {})
+utils.map("i", ".", ".<C-g>u", "Undo break points: '.'", {})
+utils.map("i", "!", "!<C-g>u", "Undo break points: '!'", {})
+utils.map("i", "?", "?<C-g>u", "Undo break points: '?'", {})
 
 -- VISUAL MODE INDENTATION
-keymap("v", "<", "<gv", full_options)
-keymap("v", ">", ">gv", full_options)
+utils.map("v", "<", "<gv", "Indentation: '<", full_options)
+utils.map("v", ">", ">gv", "Indentation: '>", full_options)
 
--- RESIZE WITH ARROWS
-keymap("n", "<C-Up>", ":resize +2<CR>", full_options)
-keymap("n", "<C-Down>", ":resize -2<CR>", full_options)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", full_options)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", full_options)
+-- RESIZE WINDOWS WITH ARROWS
+utils.map("n", "<C-Up>", ":resize +2<CR>", "Resize windows: up", full_options)
+utils.map(
+  "n",
+  "<C-Down>",
+  ":resize -2<CR>",
+  "Resize windows: down",
+  full_options
+)
+utils.map(
+  "n",
+  "<C-Left>",
+  ":vertical resize -2<CR>",
+  "Resize windows: left",
+  full_options
+)
+utils.map(
+  "n",
+  "<C-Right>",
+  ":vertical resize +2<CR>",
+  "Resize windows: right",
+  full_options
+)
